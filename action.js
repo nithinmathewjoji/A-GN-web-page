fc325938cf8775c5db95461acae577042fc6d156
Nithin Mathew Joji <76144003+nithinmathewjoji@users.noreply.github.com>
2023-11-15 20:40:09 +0530

    updated on 15-11-2023

---
+++ action.js
@@ -0,0 +1,18 @@
+
+  // Check the viewport width
+  function checkViewportWidth() {
+    if (window.innerWidth <= 500) 
+    {
+      // Redirect to another page
+      window.location.href = "warning.html";
+    }
+    else{
+        
+    }
+  }
+
+  // Check the width initially
+  checkViewportWidth();
+
+  // Add an event listener to check on window resize
+  window.addEventListener("resize", checkViewportWidth);
