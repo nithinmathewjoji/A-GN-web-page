117f8ebd285a9e802dae564bd554a7b95f3e6577
Nithin Mathew Joji <76144003+nithinmathewjoji@users.noreply.github.com>
2023-11-15 21:25:30 +0530

    disabled action.js

--- action.js
+++ action.js
@@ -1,18 +1,18 @@
 
   // Check the viewport width
-  function checkViewportWidth() {
-    if (window.innerWidth <= 500) 
-    {
+  //function checkViewportWidth() {
+    //if (window.innerWidth <= 500) 
+    //{
       // Redirect to another page
-      window.location.href = "warning.html";
-    }
-    else{
+     // window.location.href = "warning.html";
+    //}
+    //else{
         
-    }
-  }
+    //}
+  //}
 
   // Check the width initially
-  checkViewportWidth();
+  //checkViewportWidth();
 
   // Add an event listener to check on window resize
-  window.addEventListener("resize", checkViewportWidth);
+  //window.addEventListener("resize", checkViewportWidth);
